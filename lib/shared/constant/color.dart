a0cf891071315d7d9f708c0c595bd98805fff21b
Abdallah-Abdalmonem <bedo30176@gmail.com>
2023-04-10 11:46:17 +0200

    Add color & Add images

--- lib/shared/constant/color.dart
+++ lib/shared/constant/color.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color primaryColor = Color(0xffFF1100);
-  static const Color backgroundColor = Color(0xffF8F9FD);
+  static const Color primaryColor = Color(0xff3BA4AA);
+  static const Color backgroundColor = Color(0xffA3D9D9);
+  static const Color buttongreen = Color(0xff8DAA35);
+  static const Color buttonblue = Color(0xff3E5CD6);
+
   static const Color grey = Color(0xff515151);
   static const Color white = Colors.white;
   static const Color black = Colors.black;
