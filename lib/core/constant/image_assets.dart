72e438288aa3a7f4e06bdea4969f85a90f06a756
Abdallah-Abdalmonem <bedo30176@gmail.com>
2023-04-16 11:25:43 +0200

    edit image assets

--- lib/core/constant/image_assets.dart
+++ lib/core/constant/image_assets.dart
@@ -1,15 +1,11 @@
 class AppImageAsset {
+  static const String _rootImages = 'assets/images';
 
-  static const String rootImages = 'asset/images';
-
-  static const String appIcon = '$rootImages/robot.jpeg';
-  static const String onBoardingImageTwo = '$rootImages/q2.png';
-  static const String onBoardingImageThree = '$rootImages/q3.png';
-  static const String onBoardingImageFour = '$rootImages/q4.png';
+  static const String bot_Image_Blue = '$_rootImages/bot_image_blue.jpg';
+  static const String bot_Image_White = '$_rootImages/bot_image_white.jpg';
 
   // static const String rootImages = 'assets';
 
   // static const String appIcon = '$rootImages/bot_image_white.jpg';
   // static const String onBoardingImageTwo = '$rootImages/bot_image_blue.jpg';
-
 }
